8c1c2304eb69bca14ed42741680d08cf435a1904
Burak Özmen <brkzmn.wd@gmail.com>
2022-01-26 16:06:39 +0100

    Questions Shuffled
    question shuffled with collect.js

--- src/app.js
+++ src/app.js
@@ -5,10 +5,27 @@ import { initWelcomePage } from './pages/welcomePage.js';
 
 
 const loadApp = () => {
-  initWelcomePage();
-};
-const shuffleQuestion = () => {
 
+  initWelcomePage();
+  shuffleQuestions();
 };
+
+const shuffleQuestions = () => {
+  const questionIds = ["fdf"];
+
+  collect(quizData.questions).where('difficulty','easy').shuffle().slice(0,5).each((question) => {
+    questionIds.push(question.id);
+  });
+  collect(quizData.questions).where('difficulty','medium').shuffle().slice(0,5).each((question) => {
+    questionIds.push(question.id);
+  });
+  collect(quizData.questions).where('difficulty','hard').shuffle().slice(0,5).each((question) => {
+    questionIds.push(question.id);
+  })
+
+  localStorage.setItem('ids', questionIds);
+}
+
+
 window.addEventListener('load', loadApp);
 //We changed construction of loadApp
--- src/views/rightAnswer.js
+++
@@ -1,3 +0,0 @@
-import { ANSWERS_LIST_ID} from '../constants.js';
-
-export const ulElement = () => {console.log(ANSWERS_LIST_ID)};
