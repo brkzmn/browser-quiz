fc5de9b5b6a65a9eb2966822df423eba62986ef1
ensar dogan <doganermansur@icloud.com>
2022-01-26 21:39:26 +0100

    created wrong answer page

---
+++ src/pages/gameOverPage.js
@@ -0,0 +1,21 @@
+import { initWelcomePage } from './welcomePage.js';
+import { createWrongAnswerPage } from '../views/wrongAnswerView.js';
+import { USER_INTERFACE_ID } from '../constants.js';
+
+export const initGameOverPage = () => {
+    const wrongAnswerElement = createWrongAnswerPage()
+    const userInterface = document.getElementById(USER_INTERFACE_ID);
+    userInterface.appendChild(wrongAnswerElement);
+
+    document.getElementById('restart')
+    .addEventListener('click', () => {
+        userInterface.innerHTML = ''
+        setTimeout(() => {
+            initWelcomePage()
+        },500)
+        
+    })
+}
+
+
+
