5c39e8b5d74ce2b3da2fe967364ffa0a31bd4155
Burak Özmen <brkzmn.wd@gmail.com>
2022-01-27 23:54:05 +0100

    improvement-processView

--- src/views/processView.js
+++ src/views/processView.js
@@ -2,22 +2,18 @@ import { quizData } from '../data.js';
 import { prizes } from '../data.js';
 import { PROCESS_BAR_ID } from '../constants.js';
 
+
 export const initProcess = () => {
+  const processBarWrapper = document.createElement('div');
+  processBarWrapper.id = PROCESS_BAR_ID;
   const processElement = document.createElement('ul');
-  processElement.classList.add('process-bar-list');
-  quizData.questions.forEach((element, index) => {
+  processElement.classList.add('process-bar-list')
+  for(let i = 0; i < 15; i++ ) {
     const liElement = document.createElement('li');
-    liElement.textContent = `${index + 1} -- ${prizes[index]}$`;
+    liElement.textContent = `${i+ 1} -- ${prizes[i]}$`;
+    (i === quizData.currentQuestionIndex) ? liElement.classList.add('active-bar') : liElement.classList.add('disabled-bar');
     processElement.appendChild(liElement);
-  });
-  const liArray = Array.from(processElement.children);
-  liArray.forEach((item) => {
-    item.classList.add('disabled-bar');
-  });
-  liArray[quizData.currentQuestionIndex].classList.remove('disabled-bar');
-  liArray[quizData.currentQuestionIndex].classList.add('active-bar');
-  const newDiv = document.createElement('div');
-  newDiv.id = PROCESS_BAR_ID;
-  newDiv.appendChild(processElement);
-  return newDiv;
-};
+  }
+  processBarWrapper.appendChild(processElement);
+  return processBarWrapper
+}
