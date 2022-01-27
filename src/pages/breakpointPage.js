9d3a8507176f424bc7f1bd52ef13f59dc8f74687
Burak Özmen <brkzmn.wd@gmail.com>
2022-01-27 18:13:17 +0100

    breakpoint-screen
    added breakpoint screen after Q5 and Q10

--- src/pages/breakpointPage.js
+++ src/pages/breakpointPage.js
@@ -1,8 +1,31 @@
 import { getBreakpointView } from '../views/breakpointView.js';
-import { USER_INTERFACE_ID } from '../constants.js';
+import { BREAKPOINT_BUTTON_ACCEPT_ID, BREAKPOINT_BUTTON_CONTINUE_ID, USER_INTERFACE_ID } from '../constants.js';
+import { quizData } from '../data.js';
+import { initQuestionPage } from './questionPage.js';
+import { loadApp } from '../app.js';
+
+
+
 export const initBreakpointPage = (index) => {
     const breakpoint = getBreakpointView(index);  
     const userInterface = document.getElementById(USER_INTERFACE_ID);
     userInterface.appendChild(breakpoint); 
+
+    const continueBtn = document.getElementById(BREAKPOINT_BUTTON_CONTINUE_ID);
+    const acceptBtn = document.getElementById(BREAKPOINT_BUTTON_ACCEPT_ID);
+    
+    continueBtn.addEventListener('click', () => {
+        const userInterfaceElement = document.getElementById(USER_INTERFACE_ID); // why we need this???? 
+        quizData.currentQuestionIndex = quizData.currentQuestionIndex + 1;
+        userInterfaceElement.innerHTML = '';
+        initQuestionPage();
+    })
+
+    acceptBtn.addEventListener('click', () => {
+        const userInterfaceElement = document.getElementById(USER_INTERFACE_ID); // why we need this???? 
+        userInterfaceElement.innerHTML = '';
+        loadApp();
+    });
+
 
 }
