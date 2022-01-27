5d502a857a83c4fee3b5b11f79283df8d939d97f
ensar dogan <doganermansur@icloud.com>
2022-01-27 10:36:40 +0100

    modiified

--- src/views/wrongAnswerView.js
+++ src/views/wrongAnswerView.js
@@ -2,12 +2,20 @@
 import { quizData } from '../data.js';
 
 export const createWrongAnswerPage = () => {
+  const index = quizData.currentQuestionIndex
+  let prize;
+  if(index >= 5 && index < 10)
+    prize = '$ 2000';
+  if(index >= 10 && index < 15)
+    prize = '$ 15000'
+  else
+    prize = '$ 0'
   
   const element = document.createElement('div');
   // I use String.raw just to get fancy colors for the HTML in VS Code.
   element.innerHTML = String.raw`
     <h1>OPPPS! WRONG ANSWER</h1>
-   
+    <p>You won ${prize}</p>
     <button id="restart">RESTART</button>
     `;
   
