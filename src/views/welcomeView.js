e40312157859697a0345c2f834e72d86ea4c1ee3
ayktldg <aykutuludag26@gmail.com>
2022-01-29 16:34:03 +0100

    fix welcome page text

--- src/views/welcomeView.js
+++ src/views/welcomeView.js
@@ -8,8 +8,8 @@ export const initWelcome = () => {
   <img class="logo" src="../public/assets/logo_millionaire.webp" alt="game logo">
       <h1 class='welcome-title'>Game Who Wants to Be a Millionaire?</h1>
       <p>Welcome to the free online version of the award-winning game show Who Wants to Be a Millionaire? The game is open to anyone willing to put their knowledge to the test in a series of questions and answers.</p>
-      <p>The game consists of 10 questions. You have 30 seconds for each questions. After you tap on the choice, you can't change it. Answer will pop up two seconds after replying. You can take the prize at 5th and 10th question.</p>  
-      <p>If you get stuck you can call your friend to get help by clicking help button. 50/50 option eliminates the two wrong choice. There is also an option to take a hint. You can utilize help option once in all the contest. </p> 
+      <p>The game consists of 15 questions. You have 30 seconds for each questions. After you tap on the choice, you can't change it. Answer will pop up two seconds after replying. You can take the prize at 5th and 10th question.</p>  
+      <p>If you get stuck you can get help by clicking 50:50 button eliminates the two wrong choice. You can use 50:50 option once in all the contest. </p> 
       <button class='btn start-btn' id='${START_BUTTON_ID}'>Start</button>
     `;
 
